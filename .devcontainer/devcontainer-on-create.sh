1d8821b613e6600e358fd805d75cd519d53b2219
John Stairs <jostairs@microsoft.com>
2024-05-08 15:37:46 +0000

    Introduce environment variable

--- .devcontainer/devcontainer-on-create.sh
+++ .devcontainer/devcontainer-on-create.sh
@@ -1,5 +1,11 @@
 #! /usr/bin/env bash
 
-if [ -s /opt/matlab/latest/licenses/license.host.lic ]; then
-    cp /opt/matlab/latest/licenses/license.host.lic /opt/matlab/latest/licenses/license.lic
+set -euo pipefail
+
+host_license_file="/tmp/matlab.host.lic"
+
+if [ -s "${host_license_file}" ]; then
+    # We don't bind mount directly to $MATLAB_LICENSE_FILE so that we can write
+    # to it later without affecting the host.
+    cp "${host_license_file}" "${MATLAB_LICENSE_FILE}"
 fi
